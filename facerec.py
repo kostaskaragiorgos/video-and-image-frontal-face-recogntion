a8619e91ea9f01b788780d2b641f805692095fab
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-02-21 12:09:14 +0200

    Update facerec.py

--- facerec.py
+++ facerec.py
@@ -1,77 +1,72 @@
-from tkinter import *
+from tkinter import Menu, Tk
 from tkinter import messagebox as msg
 from tkinter import filedialog
-
 import cv2
-
 class FRONTAL_FACE_RECOGNTION():
-    def __init__(self,master):
+    def __init__(self, master):
         self.master = master
         self.master.title("FRONTAL FACE RECOGNTION")
         self.master.geometry("250x120")
-        self.master.resizable(False,False)
+        self.master.resizable(False, False)
 
         self.faceCascade  = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
         self.menu = Menu(self.master)
         
-        self.file_menu = Menu(self.menu,tearoff = 0)
-        self.file_menu.add_command(label = "IMAGE FACE RECOGNITION",command = self.imgrec)
-        self.file_menu.add_command(label = "VIDEO FACE RECOGNITION",command = self.vidrec)
-        self.file_menu.add_command(label="Exit",accelerator= 'Alt+F4',command = self.exitmenu)
-        self.menu.add_cascade(label = "File",menu=self.file_menu)
+        self.file_menu = Menu(self.menu, tearoff = 0)
+        self.file_menu.add_command(label = "IMAGE FACE RECOGNITION", command = self.imgrec)
+        self.file_menu.add_command(label = "VIDEO FACE RECOGNITION", command = self.vidrec)
+        self.file_menu.add_command(label="Exit",accelerator= 'Alt+F4', command = self.exitmenu)
+        self.menu.add_cascade(label = "File", menu=self.file_menu)
         
-        self.about_menu = Menu(self.menu,tearoff = 0)
-        self.about_menu.add_command(label = "About",accelerator= 'Ctrl+I',command=self.aboutmenu)
-        self.menu.add_cascade(label="About",menu=self.about_menu)
+        self.about_menu = Menu(self.menu, tearoff = 0)
+        self.about_menu.add_command(label = "About", accelerator= 'Ctrl+I', command=self.aboutmenu)
+        self.menu.add_cascade(label="About", menu=self.about_menu)
         
-        self.help_menu = Menu(self.menu,tearoff = 0)
-        self.help_menu.add_command(label = "Help",accelerator = 'Ctrl+F1',command=self.helpmenu)
-        self.menu.add_cascade(label="Help",menu=self.help_menu)
+        self.help_menu = Menu(self.menu, tearoff = 0)
+        self.help_menu.add_command(label = "Help", accelerator = 'Ctrl+F1', command=self.helpmenu)
+        self.menu.add_cascade(label="Help", menu=self.help_menu)
         
         
         self.master.config(menu=self.menu)
-        self.master.bind('<Alt-F4>',lambda event: self.exitmenu())
-        self.master.bind('<Control-F1>',lambda event: self.helpmenu())
-        self.master.bind('<Control-i>',lambda event:self.aboutmenu())
+        self.master.bind('<Alt-F4>', lambda event: self.exitmenu())
+        self.master.bind('<Control-F1>', lambda event: self.helpmenu())
+        self.master.bind('<Control-i>', lambda event:self.aboutmenu())
         
     def imgrec(self):
         imgfile = filedialog.askopenfilename(initialdir ="/",title ="Select an image file",
                                              filetypes =(("image files","*.jpg"),("all files","*.*")))
         if ".jpg" in imgfile:
             image = cv2.imread(imgfile)
-            gray = cv2.cvtColor(image , cv2.COLOR_BGR2GRAY)
-            faceRects = self.faceCascade.detectMultiScale(gray,scaleFactor = 1.2,minNeighbors = 6, minSize=(30,30))
-            for (x,y,w,h) in faceRects:
-                cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
-            cv2.imshow("Faces",image)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            faceRects = self.faceCascade.detectMultiScale(gray, scaleFactor = 1.2, minNeighbors = 6, minSize=(30, 30))
+            for (x, y, w, h) in faceRects:
+                cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.imshow("Faces", image)
             cv2.waitKey(0)
-            msg.showinfo("FACES FOUND","FACES FOUND:"+str(len(faceRects)))
+            msg.showinfo("FACES FOUND", "FACES FOUND:"+str(len(faceRects)))
         else:
-            msg.showerror("Abort" , "Abort")
-
-        
+            msg.showerror("Abort", "Abort")
     def vidrec(self):
-        videofile = filedialog.askopenfilename(initialdir = "/",title = "Select a video file",
-                                               filetypes = (("video files", "*.mp4"),("all files","*.*")))
-        
+        videofile = filedialog.askopenfilename(initialdir = "/", title = "Select a video file",
+                                               filetypes = (("video files", "*.mp4"), ("all files", "*.*")))
         if ".mp4" in videofile:
             camera =cv2.VideoCapture(videofile)
         
             while True:
-                (check,frame) = camera.read()
-                frame = cv2.resize(frame ,(400,400),400,0)
+                (check, frame) = camera.read()
+                frame = cv2.resize(frame, (400, 400), 400,0)
                 gray  = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-                faceRects = self.faceCascade.detectMultiScale(gray,scaleFactor = 1.2,minNeighbors = 6, minSize=(30,30))
-                for (x,y,w,h) in faceRects:
-                    cv2.rectangle(frame,(x,y), (x+w,y+h),(0,255,0),2)
+                faceRects = self.faceCascade.detectMultiScale(gray, scaleFactor = 1.2, minNeighbors = 6, minSize=(30, 30))
+                for (x, y, w, h) in faceRects:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
             
-                cv2.imshow("Face",frame)
+                cv2.imshow("Face", frame)
                 if cv2.waitKey(1) & 0xFF == ord("q"):
                     break
             camera.release()
             cv2.destroyAllWindows()
         else:
-            msg.showerror("Abort" , "Abort")
+            msg.showerror("Abort", "Abort")
             
     def exitmenu(self):
         if msg.askokcancel("Quit?", "Really quit?"):
@@ -81,13 +76,11 @@ class FRONTAL_FACE_RECOGNTION():
         msg.showinfo("HELP", "HELP \n 1. Choose from the menu \n 2. Import a file")
     
     def aboutmenu(self):
-        msg.showinfo("About","About \nVersion 1.0")
-     
-
+        msg.showinfo("About", "About \nVersion 1.0")
 def main():
     root=Tk()
-    FFR = FRONTAL_FACE_RECOGNTION(root)
+    FRONTAL_FACE_RECOGNTION(root)
     root.mainloop()
     
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
