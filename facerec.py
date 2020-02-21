561a1579a9e29c8dcb24c311e814f574b45089fa
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-02-21 12:14:22 +0200

    Update facerec.py

--- facerec.py
+++ facerec.py
@@ -16,24 +16,19 @@ class FRONTAL_FACE_RECOGNTION():
         self.master.geometry("250x120")
         self.master.resizable(False, False)
 
-        self.faceCascade  = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+        self.faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
         self.menu = Menu(self.master)
-        
-        self.file_menu = Menu(self.menu, tearoff = 0)
-        self.file_menu.add_command(label = "IMAGE FACE RECOGNITION", command = self.imgrec)
-        self.file_menu.add_command(label = "VIDEO FACE RECOGNITION", command = self.vidrec)
-        self.file_menu.add_command(label="Exit",accelerator= 'Alt+F4', command = self.exitmenu)
-        self.menu.add_cascade(label = "File", menu=self.file_menu)
-        
-        self.about_menu = Menu(self.menu, tearoff = 0)
-        self.about_menu.add_command(label = "About", accelerator= 'Ctrl+I', command=self.aboutmenu)
+        self.file_menu = Menu(self.menu, tearoff=0)
+        self.file_menu.add_command(label="IMAGE FACE RECOGNITION", command = self.imgrec)
+        self.file_menu.add_command(label="VIDEO FACE RECOGNITION", command = self.vidrec)
+        self.file_menu.add_command(label="Exit", accelerator= 'Alt+F4', command = self.exitmenu)
+        self.menu.add_cascade(label="File", menu=self.file_menu)
+        self.about_menu = Menu(self.menu, tearoff=0)
+        self.about_menu.add_command(label="About", accelerator= 'Ctrl+I', command=self.aboutmenu)
         self.menu.add_cascade(label="About", menu=self.about_menu)
-        
-        self.help_menu = Menu(self.menu, tearoff = 0)
-        self.help_menu.add_command(label = "Help", accelerator = 'Ctrl+F1', command=self.helpmenu)
+        self.help_menu = Menu(self.menu, tearoff=0)
+        self.help_menu.add_command(label="Help", accelerator = 'Ctrl+F1', command=self.helpmenu)
         self.menu.add_cascade(label="Help", menu=self.help_menu)
-        
-        
         self.master.config(menu=self.menu)
         self.master.bind('<Alt-F4>', lambda event: self.exitmenu())
         self.master.bind('<Control-F1>', lambda event: self.helpmenu())
